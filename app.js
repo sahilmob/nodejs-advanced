0b7703219ec7850e168a3ccfeed4ffbb5f921294
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-03 18:30:27 +0200

    convert html to ejs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
