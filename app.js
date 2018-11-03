fc0bfe67471028be7bb66a8e4fb03a5340ac5897
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-03 18:43:57 +0200

    use ejs partials

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: "Page not found" })
+    res.status(404).render('404', { pageTitle: "Page not found", path: '' })
 });
 
 app.listen(3000);
